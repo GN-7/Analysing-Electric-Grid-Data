64f354195800b8855ff42499ee34cfb8cb8834bb
Nipun G <nipun.gadapa@gmail.com>
2026-07-08 09:09:41 +0530

    Performance Optimisation, Successful sorting by Year and Region

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
-
+import datetime
 #Accessing The Data
 base_dir = Path(__file__).parent
 df = pd.read_excel(f"{base_dir}/MainData.xlsx")
@@ -11,24 +11,17 @@ df = pd.read_excel(f"{base_dir}/MainData.xlsx")
 
 df['Dates'] = pd.to_datetime(df['datetime']).dt.date
 df['Time'] = pd.to_datetime(df['datetime']).dt.time
-df = df.drop(columns="datetime")
+
 
 #Pivot Tables for each Region
-west_pivoted = pd.pivot_table(df, index=["Dates"], columns="Time", values="West", aggfunc="mean", margins="True")
-east_pivoted = pd.pivot_table(df, index=["Dates"], columns="Time", values="East", aggfunc="mean", margins="True")
-north_pivoted = pd.pivot_table(df, index=["Dates"], columns="Time", values="North", aggfunc="mean", margins="True")
-south_pivoted = pd.pivot_table(df, index=["Dates"], columns="Time", values="South", aggfunc="mean", margins="True")
-national_pivoted = pd.pivot_table(df, index=["Dates"], columns="Time", values="National", aggfunc="mean", margins="True")
-northeast_pivoted = pd.pivot_table(df, index=["Dates"], columns="Time", values="North East", aggfunc="mean", margins="True")
+region = "National" # East, West, North, South, National, North East
+year = 2020         #2019, 20, 21, 22, 23, 24
+pivoted_region = pd.pivot_table(df, index=["Dates"], columns="Time", values=region)
 
+df_region_year = pivoted_region.loc[datetime.date(year, 1,1):datetime.date(year, 12, 31)] #DataFrame not Pivot Table
 
-#Plotting
-hours = pd.date_range("01-01-2019", "01-02-2019", freq="h", inclusive="left").hour.astype(str)
-plt.plot(hours, west_pivoted.loc["All"][0:-1])
-plt.xlabel("Hour of the Day (24 Hour Format)")
-plt.ylabel("Average load over 5 yr interval")
-plt.xticks(hours)
+avg_day_in_region_in_year = df_region_year.aggregate("mean", axis=0)
+ticks = pd.date_range("01-01-2019", "01-02-2019", freq="h", inclusive="left").hour.astype(str)
+plt.plot(ticks, avg_day_in_region_in_year)
+plt.xticks(ticks, rotation="vertical")
 plt.show()
-
-
-
