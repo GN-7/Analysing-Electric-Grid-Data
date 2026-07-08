8fd77a89bed340b14fefec92a2674930159d0d39
Nipun G <nipun.gadapa@gmail.com>
2026-07-08 10:34:33 +0530

    Flexible Time Interval Functionality

--- main.py
+++ main.py
@@ -15,13 +15,18 @@ df['Time'] = pd.to_datetime(df['datetime']).dt.time
 
 #Pivot Tables for each Region
 region = "National" # East, West, North, South, National, North East
-year = 2020         #2019, 20, 21, 22, 23, 24
+start_date = (2019, 1, 1) #(year, month, day)
+end_date = (2024, 4, 30)
 pivoted_region = pd.pivot_table(df, index=["Dates"], columns="Time", values=region)
 
-df_region_year = pivoted_region.loc[datetime.date(year, 1,1):datetime.date(year, 12, 31)] #DataFrame not Pivot Table
+#DataFrame not Pivot Table
+df_region_interval = pivoted_region.loc[datetime.date(*start_date):datetime.date(*end_date)] 
+avg_day_in_region_in_interval = df_region_interval.aggregate("mean", axis=0)
 
-avg_day_in_region_in_year = df_region_year.aggregate("mean", axis=0)
+
+
+#---------PLOTTING---------------
 ticks = pd.date_range("01-01-2019", "01-02-2019", freq="h", inclusive="left").hour.astype(str)
-plt.plot(ticks, avg_day_in_region_in_year)
-plt.xticks(ticks, rotation="vertical")
+plt.plot(ticks, avg_day_in_region_in_interval)
+plt.xticks(ticks)
 plt.show()
