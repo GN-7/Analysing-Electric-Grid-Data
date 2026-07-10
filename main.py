31578b437a098f3c8a879d16db8505436f3ae81c
Nipun G <nipun.gadapa@gmail.com>
2026-07-10 10:24:14 +0530

    Successful Merging and Importing Functions

---
+++ main.py
@@ -0,0 +1,5 @@
+from average_day_curves import average_day_curves
+from normalized_day_curves import normalized_day_curves
+
+average_day_curves(["National", "National"], [2019, 2020])
+normalized_day_curves(["National", "National"], [2019, 2020])
