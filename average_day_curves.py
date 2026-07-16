41116ca3366854898d7ebcbdb2a677eaa54acaef
Nipun G <nipun.gadapa@gmail.com>
2026-07-16 12:53:46 +0530

    Plot Customization in Average Day Curves

--- average_day_curves.py
+++ average_day_curves.py
@@ -41,7 +41,7 @@ def average_day_curves(region_list : list, year_list: list):
 
     #---------PLOTTING---------------
     ticks = pd.date_range("01-01-2019", "01-02-2019", freq="h", inclusive="left").hour.astype(str)
-    colors = {2019: "Blue", 2020: "Orange", 2021: "Green", 2022: "Maroon", 2023: "Purple"}
+    colors = {2019: "#34558B", 2020: "#BA7517", 2021: "#1D5C4C", 2022: "#93373F", 2023: "#6E3D63"}
     #----------THE ACTUAL PLOTS------------
     fig, ax = plt.subplots(figsize=(8, 4.5), layout='constrained')
     for year, plot in zip(year_list ,plots_list, strict=True):
@@ -51,12 +51,12 @@ def average_day_curves(region_list : list, year_list: list):
 
     #---------PLOT CUSTOMIZATION------------------    
 
-    plt.title("Average Day of given years in given regions")
-    ax.set_title("India's average day keeps similar shape while demand grows")
-    ax.set_xlabel('Hour of day')
-    ax.set_ylabel('Average Demand (GW)')
-    ax.set_xlim(0, 25.6)
-    ax.set_xticks(ticks)
+    plt.title("Average Day of given years in given regions", fontweight="bold")
+    plt.xlabel('Hour of day', fontweight="bold")
+    plt.ylabel('Average Demand (MW)', fontweight="bold")
+    plt.xlim(0, 25.6)
+    plt.xticks(ticks)
+    plt.grid(axis="y", alpha=0.25, linewidth=0.6, linestyle="dashed")
     plt.show()
 
 if __name__ == "__main__":
